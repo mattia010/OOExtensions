ea493a2c9c37d11510d03e5e82bf5cd98d056e17
Mattia <mat.bolo10@gmail.com>
2023-12-30 02:22:11 +0100

    Completely recoded. All features imllpemented. Method invocation is bugged.

--- Prolog/oop.pl
+++ Prolog/oop.pl
@@ -3,118 +3,157 @@
 %%%% Bolognini Mattia 870401
 %%%% Nessuna collaborazione
 
+% DA FINIRE: COMPLETARE SOLO FUNZIONI TRAMPOLINO PER LA CHIAMATA AI METODI
+% E GESTIRE CORRETTAMENTE LE CHIAMATE AI METODI.
+% assert_trampolines, invoke_method, get_best_method,
+% get_method_from_methods_list
 
 
-%%% TERMINI COMPOSTI UTILIZZATI %%%
-
-% Classi = class(ClassName, FieldsList, MethodsList),
-
-% Metodi = method(Name, ArgsList, Body).
-
-% Campi = field(Name, Object).
-
-% Oggetti = object(Type, Value) per i tipi builtin.
-%           object(Type, FieldsList) per i tipi userdefined (classi).
-% Type void definisce un oggetto non tipizzato.
-% Un oggetto deve avere sempre un valore iniziale,
-% non può rimanere indefinito.
-
-% Puntatori a oggetti = point_to(PtrName, Object).
-% Un puntatore point_to(PtrName, nullptr) è un puntatore che non punta a
-% nessun oggetto.
-
-% Ereditarietà tra classi = child_of(ChildName, ParentName).
-
-%SISTEMARE STRING
-
-class(object, [], []).
+class(object, [], [], []).
 point_to(ptr, nullptr).
-:- dynamic class/3.
+:- dynamic class/4.
 :- dynamic point_to/2.
 
-% Definisce una nuova classe, ovvero crea fatti e regole per esprimere
-% relazioni di ereditarietà, metodi e campi della classe.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% PREDICATI PER LA DEFINIZIONE DI UNA CLASSE %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 def_class(ClassName, Parents) :-
-    forall(member(Parent, Parents), class(Parent, _, _)),
-    forall(member(Parent, Parents), asserta(child_of(ClassName, Parent))).
+    def_class(ClassName, Parents, []).
 def_class(ClassName, Parents, Parts) :-
-    \+ class(ClassName, _, _),
-    forall(member(Parent, Parents), class(Parent, _, _)),
-    forall(member(Parent, Parents),
-	   asserta(child_of(ClassName, Parent))),
-    get_methods(Parts, Methods),
-    get_fields(Parts, Fields),
-    inherit(Methods, Fields, Parents, AllMethods, AllFields),
-    forall(member(Method, AllMethods),
-	   assert_method(ClassName, Method)),
-    asserta(class(ClassName, AllFields, AllMethods)).
+    \+ class(ClassName, _, _, _),
+    forall(member(Parent, Parents), class(Parent, _, _, _)),
+    get_methods(Parts, TmpMethods),
+    get_fields(Parts, TmpFields),
+    standardize_fields(TmpFields, StandardFields),
+    standardize_methods(TmpMethods, StandardMethods),
+    assert_trampolines(StandardMethods),
+    asserta(class(ClassName, Parents, StandardFields, StandardMethods)).
 
-
-inherit(Methods, Fields, [], Methods, Fields).
-inherit(Methods, Fields, [Parent | Rest], AllMethods, AllFields) :-
-    class(Parent, ParentFields, ParentMethods),
-    inherit(Methods, Fields, Rest, RestMethods, RestFields),
-    append(ParentFields, RestFields, AllFields),
-    append(ParentMethods, RestMethods, AllMethods).
-
-get_methods([], []).
-get_methods([method(Name, Params, Body) | Rest],
-	    [method(Name, Params, Body) | MethodRest]) :-
-    get_methods(Rest, MethodRest).
-get_methods([field(_, _, _) | Rest], Result) :-
+get_methods([], []) :- !.
+get_methods([Field | Rest], Result) :-
+    Field =.. [field | _],
+    !,
     get_methods(Rest, Result).
-get_methods([field(_, _) | Rest], Result) :-
-    get_methods(Rest, Result).
+get_methods([Method | Rest], [Method | Tmp]) :-
+    Method =.. [method | _],
+    !,
+    get_methods(Rest, Tmp).
 
-get_fields([], []).
-get_fields([field(Name, Value) | Rest], Result) :-
-    get_fields([field(Name, Value, void) | Rest], Result).
-get_fields([field(Name, Value, Type) | Rest],
-	   [field(Name, Object) | FieldsRest]) :-
-    new(Type, Value, Object),
-    get_fields(Rest, FieldsRest).
-get_fields([method(_, _, _) | Rest], Result) :-
+get_fields([], []) :- !.
+get_fields([Method | Rest], Result) :-
+    Method =.. [method | _],
+    !,
     get_fields(Rest, Result).
+get_fields([Field | Rest], [Field | Tmp]) :-
+    Field =.. [field | _],
+    !,
+    get_fields(Rest, Tmp).
 
-assert_method(ClassName, method(MethodName, Parameters, BadBody)) :-
-    replace(this, This, BadBody, GoodBody),
-    Head =.. [MethodName, This | Parameters],
-    asserta((Head :- is_instance(This, ClassName), call(GoodBody))).
 
-%replace(Replacee, Replacement, OldTerm, NewTerm) :-
-%        (   OldTerm == Replacee -> NewTerm = Replacement
-%        ;   var(OldTerm) -> NewTerm = OldTerm
-%        ;   OldTerm =.. [Functor | OldClauses],
-%            maplist(replace(Replacee, Replacement), OldClauses, NewClauses),
-%            NewTerm =.. [Functor | NewClauses]
-%        ).
+% OK TESTATO
+standardize_fields([], []) :- !.
+standardize_fields([field(Name, Value) | Rest],
+		   [field(Name, Obj) | StandardRest]) :-
+    !,
+    new(void, Value, Obj),
+    standardize_fields(Rest, StandardRest).
+standardize_fields([field(Name, Value, Type) | Rest],
+		   [field(Name, Obj) | StandardRest]) :-
+    !,
+    new(Type, Value, Obj),
+    standardize_fields(Rest, StandardRest).
+
+standardize_methods([], []) :- !.
+standardize_methods([method(Name, Args, Body) | Rest],
+		    [(StandardHead :- StandardBody) | StandardRest]) :-
+    StandardHead =.. [Name, This | Args],
+    replace(this, This, Body, StandardBody),
+    standardize_methods(Rest, StandardRest).
 
 replace(Replacee, Replacement, Replacee, Replacement) :- !.
-replace(_, _, VariableTerm, VariableTerm) :-
-    var(VariableTerm),
-    !.
+replace(_, _, VariableTerm, VariableTerm) :- var(VariableTerm), !.
 replace(Replacee, Replacement, OldTerm, NewTerm) :-
     OldTerm =.. [Functor | OldClauses],
     maplist(replace(Replacee, Replacement), OldClauses, NewClauses),
     NewTerm =.. [Functor | NewClauses].
 
 
-retract_class(ClassName) :-
-    class(ClassName, _, _),
-    retract(class(ClassName, _, _)).
-    %MANCA DA FARE RETRACT DEI METODI DI UNA CLASSE.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% PREDICATI PER L'ISTANZIAZIONE %%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-% Istanzia un oggetto e crea un puntatore ad esso. OK
+% Istanzia un oggetto e aggiunge alla base di conoscenza un puntatore ad esso.
 make(Name, Type) :-
-    make(Name, Type, []).
+    \+ point_to(Name, _),
+    new(Type, Obj),
+    asserta(point_to(Name, Obj)).
 make(Name, Type, Params) :-
     \+ point_to(Name, _),
     new(Type, Params, Obj),
     asserta(point_to(Name, Obj)).
 
+% Eredita campi e metodi, implementando un'ereditarietà Depth-first left-most.
+inherit([], [], []) :- !.
+inherit([Parent | Rest], Fields, Methods) :-
+    !,
+    class(Parent, Grandpas, ParentFields, ParentMethods),
+    inherit(Grandpas, GrandpasFields, GrandpasMethods),
+    helper_append_fields(ParentFields, GrandpasFields, TmpFields),
+    helper_append_methods(ParentMethods, GrandpasMethods, TmpMethods),
+    inherit(Rest, RestFields, RestMethods),
+    helper_append_fields(TmpFields, RestFields, Fields),
+    helper_append_methods(TmpMethods, RestMethods, Methods).
+
+helper_append_fields([], [], []) :- !.
+helper_append_fields(X, [], X) :- X \= [], !.
+helper_append_fields([], X, X) :- X \= [], !.
+helper_append_fields([field(Name, Value) | Rest1],
+		     [field(Name, _) | Rest2],
+		     Result) :-
+    !,
+    helper_append_fields([field(Name, Value) | Rest1], Rest2, Result).
+helper_append_fields([field(Name1, Value1) | Rest1],
+		     [field(Name2, Value2) | Rest2],
+		     Result) :-
+    Name1 \= Name2,
+    !,
+    helper_append_fields(Rest1, [field(Name2, Value2)], Tmp1),
+    helper_append_fields([field(Name1, Value1) | Tmp1], Rest2, Result).
+
+% DA FARE
+helper_append_methods([], [], []) :- !.
+helper_append_methods(X, [], X) :- X \= [], !.
+helper_append_methods([], X, X) :- X \= [], !.
+helper_append_methods([(Head1 :- Body1) | Rest1],
+		      [(Head2 :- _) | Rest2],
+		      [(Head1 :- Body1) | Rest3]) :-
+    Head1 =.. [MethodName | _],
+    Head2 =.. [MethodName | _],
+    !,
+    helper_append_methods([(Head1 :- Body1) | Rest1], Rest2, Rest3).
+helper_append_methods([(Head1 :- Body1) | Rest1],
+		      [(Head2 :- Body2) | Rest2],
+		      Result) :-
+    Head1 =.. [MethodName1 | _],
+    Head2 =.. [MethodName2 | _],
+    MethodName1 \= MethodName2,
+    !,
+    helper_append_methods(Rest1, [(Head2 :- Body2)], Tmp1),
+    helper_append_methods([(Head1 :- Body1) | Tmp1], Rest2, Result).
 
 % Crea un oggetto del tipo specificato.
+new(void, object(void, null)) :- !.
+new(bool, object(bool, false)) :- !.
+new(float, object(float, 0.0)) :- !.
+new(string, object(string, "")) :- !.
+new(integer, object(integer, 0)) :- !.
+new(Type, object(Type, DefaultFields)) :-
+    is_class(Type),
+    !,
+    inherit([Type], DefaultFields, _).
+
 new(void, X, object(void, X)) :- !.
 new(bool, true, object(bool, true)) :- !.
 new(bool, false, object(bool, false)) :- !.
@@ -122,14 +161,14 @@ new(float, X, object(float, X)) :- !, float(X).
 new(string, X, object(string, X)) :- !, string(X).
 new(integer, X, object(integer, X)) :- !, integer(X).
 new(Type, Params, object(Type, Fields)) :-
-    \+ is_builtin(Type),
-    !,
-    class(Type, DefaultFields, _),
+    is_class(Type),
+    inherit([Type], DefaultFields, _),
     init_fields(DefaultFields, Params, Fields).
 
 % Inizializza i campi nella costruzione di un oggetto di una classe. OK
-init_fields(DefaultFields, [], DefaultFields).
+init_fields(DefaultFields, [], DefaultFields) :- !.
 init_fields(DefaultFields, [FirstParam | Rest], Fields) :-
+    !,
     compound_name_arguments(FirstParam, =, [FieldName, NewValue]),
     member(field(FieldName, object(Type, _)), DefaultFields),
     is_instance(object(Type, NewValue)),
@@ -138,35 +177,32 @@ init_fields(DefaultFields, [FirstParam | Rest], Fields) :-
 		Rest,
 		Fields).
 
-% Distrugge un'oggetto e il puntatore.  OK
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% PREDICATI UTILS %%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Distrugge un'oggetto e il puntatore ad esso (solo se presenti),
+% rimuovendoli dalla base di conoscenza.
 free(Ptr) :-
     point_to(Ptr, _),
     retract(point_to(Ptr, _)).
 
-
+% DA SISTEMARE PER L'EREDITARIETA
 % Ottiene il valore del campo dell'istanza di una classe.   OK
 field(Ptr, Name, Value) :-
-    point_to(Ptr, object(Type, Fields)),
-    is_class(Type),
-    member(field(Name, object(_, Value)), Fields).
+    inst(Ptr, Obj),
+    !,
+    field(Obj, Name, Value).
+field(object(_, [field(Name, object(_, Value)) | _]), Name, Value) :- !.
+field(object(_, [field(FieldName, _) | FieldsRest]), Name, Value) :-
+    FieldName \= Name,
+    !,
+    field(object(_, FieldsRest), Name, Value).
 
-% Aggiorna un campo di un'istanza di una classe con un nuovo valore.  OK
-set_field(Ptr, Name, Value) :-
-    point_to(Ptr, object(Type, OldFields)),
-    is_class(Type),
-    member(field(Name, _), OldFields),
-    delete(OldFields, field(Name, object(FieldType, _)), Tmp),
-    is_instance(object(FieldType, Value)),
-    retract(point_to(Ptr, _)),
-    asserta(
-	point_to(Ptr,
-		 object(Type,
-			[field(Name,
-			       object(FieldType, Value)) | Tmp]))).
-    
 
 % Verifica l'esistenza di una classe.
-is_class(Type) :- class(Type, _, _).
+is_class(Type) :- class(Type, _, _, _).
 
 % Ritorna l'oggetto puntato da un puntatore.
 inst(Ptr, Obj) :- point_to(Ptr, Obj).
@@ -196,114 +232,68 @@ is_builtin(float).
 is_builtin(string).
 is_builtin(integer).
 
+retract_class(ClassName) :-
+    class(ClassName, _, _, _),
+    retract(class(ClassName, _, _, _)).
 
-%%% TESTS
-:- begin_tests(is_instance).
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%% DA RIFINIRE MA FUNZIONANTI %%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-test(is_instance_bool_true) :-
-    % manca il test per is_instance con classi.
-    is_instance(object(bool, true)).
-test(not_is_instance_bool) :-
-    \+ is_instance(object(bool, cur)).
-test(is_instance_bool_false) :-
-    is_instance(object(bool, false)).
-test(is_instance_string1) :-
-    is_instance(object(string, "cduri")).
-test(is_instance_string2) :-
-    is_instance(object(string, "a")).
-test(is_instance_integer1) :-
-    is_instance(object(integer, 234)).
-test(is_instance_integer2) :-
-    is_instance(object(integer, 34)).
-test(not_is_instance_integer1) :-
-    \+ is_instance(object(integer, as)).
-test(not_is_instance_integer2) :-
-    \+ is_instance(object(integer, 34.45)).
-test(is_instance_float1) :-
-    is_instance(object(float, 3.4)).
-test(is_instance_float2) :-
-    is_instance(object(float, 4.0)).
-test(is_not_instance_float) :-
-    \+ is_instance(object(float, e)).
-test(is_instance_void1) :-
-    is_instance(object(void, 45)).
-test(is_instance_void2) :-
-    is_instance(object(void, as)).
+assert_trampolines([]).
+assert_trampolines([(Head :- _) | Rest]) :-
+    Head =.. [Name | Args],
+    asserta((Head :- invoke_method(Name, Args))),
+    assert_trampolines(Rest).
 
-:- end_tests(is_instance).
+invoke_method(Name, [Ptr | Args]) :-
+    inst(Ptr, Instance),
+    invoke_method(Name, [Instance | Args]),
+    !.
+invoke_method(Name, [object(Type, Value) | Args]) :-
+    !,
+    get_best_method([Type], Name, Method),
+    asserta(Method),
+    CalledHead =.. [Name | [object(Type, Value) | Args]],
+    call(CalledHead),
+    !, retract(Method).
 
+get_best_method([Class], MethodName, BestMethod) :-
+    class(Class, _, _, MethodsList),
+    get_method_from_methods_list(MethodName, MethodsList, BestMethod),
+    !.
+get_best_method([Class], MethodName, BestMethod) :-
+    class(Class, Parents, _,  MethodsList),
+    \+ get_method_from_methods_list(MethodName, MethodsList, _),
+    !,
+    get_best_method(Parents, MethodName, BestMethod).
+get_best_method([Class | Classes], MethodName, BestMethod) :-
+    Classes \= [],
+    get_best_method([Class], MethodName, BestMethod),
+    !.
+get_best_method([Class | Classes], MethodName, BestMethod) :-
+    Classes \= [],
+    \+ get_best_method([Class], MethodName, _),
+    class(Class, Parents, _, _),
+    get_best_method(Parents, MethodName, BestMethod),
+    !.
+get_best_method([Class | Classes], MethodName, BestMethod) :-
+    Classes \= [],
+    \+ get_best_method([Class], MethodName, _),
+    class(Class, Parents, _, _),
+    \+ get_best_method(Parents, MethodName, _),
+    get_best_method(Classes, MethodName, BestMethod),
+    !.
 
-:- begin_tests(instances).
-
-test(def_class_only_fields) :-
-    def_class(astudent, [],[field(name, 'Eva Lu Ator' ),
-			   field(university, 'Berkeley')]),
-    retract_class(astudent).
-
-test(def_class_with_method) :-
-    def_class(bstudent, [],[field(name, 'Eva Lu Ator' ),
-				 field(university, 'Berkeley'),
-				 method(talk, [],
-					(write('My name is '),
-					 field(this, name, N),
-					 writeln(N),
-					 write('My age is '),
-					 field(this, age, A),
-					 writeln(A)))
-			  ]),
-    retract_class(bstudent).
-
-test(make_instance) :-
-    def_class(cstudent, [],[field(name, 'Eva Lu Ator' ),
-				 field(university, 'Berkeley'),
-				 method(talk, [],
-					(write('My name is '),
-					 field(this, name, N),
-					 writeln(N),
-					 write('My age is '),
-					 field(this, age, A),
-					 writeln(A)))
-				]),
-    make(s1, cstudent, [name = 108]),
-    free(s1),
-    retract_class(cstudent).
-:- end_tests(instances).
-
-
-
-:- begin_tests(inheritance).
-
-test(inheritance_1) :-
-    def_class(person, [], [field(name, 'Eve'), field(age, 21, integer)]),
-    \+ def_class(person, [], [field(name, 'Eve'), field(age, 21, integer)]),
-    def_class(student, [person], [field(name, 'Eva Lu Ator'),
-				  field(university, 'Berkeley'),
-				  method(talk, [],
-					 (write('My name is '),
-					  field(this, name, N),
-					  writeln(N),
-					  write('My age is '),
-					  field(this, age, A),
-					  writeln(A)))]),
-    class(student, _, _),
-    class(person, _, _),
-    make(eve, person),
-    make(adam, person, [name = 'Adam']),
-    make(s1, student, [name = 'Eduardo De Filippo', age = 108]),
-    make(s2, student),
-    point_to(s2, _),
-    point_to(s1, object(student, _)),
-    \+ make(s3, student, [name = 'Harry Potter', age = "12"]),
-    \+ point_to(s3, _),
-    field(eve, age, 21),
-    field(s1, age, 108),
-    field(s2, name, 'Eva Lu Ator'),
-    \+ field(eve, age, 234),
-    free(eve),
-    free(adam),
-    free(s1),
-    free(s2),
-    retract_class(person),
-    retract_class(student).
-
-:- end_tests(inheritance).
+% OK TESTATO   
+get_method_from_methods_list(MethodName,
+			     [(Head :- Body) | _],
+			     (Head :- Body)) :-
+    Head =.. [MethodName | _].
+get_method_from_methods_list(MethodName,
+			     [(Head :- _) | Rest],
+			     Result) :-
+    Head =.. [Functor | _],
+    Functor \= MethodName,
+    get_method_from_methods_list(MethodName, Rest, Result).
+    
